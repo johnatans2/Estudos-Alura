744836bdf367fa09199bbe3708b72647967c1a91
johnatans2 <johnatan.henrique14@hotmail.com>
2024-03-26 20:19:48 -0300

    Desafio 1 do curso de Lógica de Programação

--- Lógica de Programação/app.js
+++ Lógica de Programação/app.js
@@ -1,3 +1,13 @@
-window.alert("Bem vindo ao jogo do número secreto");
-let NumeroSecreto=29;
-window.prompt("Digite um número entre 1 e 10");
+window.alert("Boas vindas ao nosso site!");
+let nome="Lua";
+let idade=25;
+let numeroDeVendas=50;
+let saldoDisponivel=1000;
+window.alert("Erro! Peencha todos os campos");
+let mensagemDeErro="Erro! Preencha todos os campos";
+window.alert(mensagemDeErro);
+nome=window.prompt("Qual é o seu nome?");
+idade=window.prompt("Digite sua idade:");
+if(idade>18){
+    window.alert("Pode tirar a habilitação!");
+};
