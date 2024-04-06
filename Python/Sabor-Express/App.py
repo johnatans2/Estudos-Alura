d56ca54a7bbfdb6ae3c4880b13bf66704ce2be1f
johnatans2 <johnatan.henrique14@hotmail.com>
2024-04-06 10:26:42 -0300

    Melhorando o Código em Python

--- Python/Sabor-Express/App.py
+++ Python/Sabor-Express/App.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+print("""
+      𝒮𝒶𝒷ℴ𝓇 ℰ𝓍𝓅𝓇ℯ𝓈𝓈
+      
+1 - Cadastrar Restaurante
+2 - Listar Restaurantes
+3 - Ativar Restaurante
+4 - Sair
+""")
+
+opcaoEscolhida=input("Digite a Opação Escolhida: ")
+print(f"Você escohei a opção {opcaoEscolhida}")
