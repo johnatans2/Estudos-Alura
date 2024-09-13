16568fcbfabab34eba557b47331ff0c608f8ffac
johnatans2 <johnatan.henrique14@hotmail.com>
2024-09-12 23:23:45 -0300

    Concluindo esse desafio

--- Lógica de Programação/app.js
+++ Lógica de Programação/app.js
@@ -1,13 +1,14 @@
-window.alert("Boas vindas ao nosso site!");
-let nome="Lua";
-let idade=25;
-let numeroDeVendas=50;
-let saldoDisponivel=1000;
-window.alert("Erro! Peencha todos os campos");
-let mensagemDeErro="Erro! Preencha todos os campos";
-window.alert(mensagemDeErro);
-nome=window.prompt("Qual é o seu nome?");
-idade=window.prompt("Digite sua idade:");
-if(idade>18){
-    window.alert("Pode tirar a habilitação!");
-};
+//Declaração de Variáveis
+let limiteNumeroSecreto=100;
+let numeroSecreto=parseInt(Math.random()*limiteNumeroSecreto+1);
+let palpite;
+//Processamento de Dados e Input
+do{
+    window.alert("Olá, seja muito bem vindo ao jogo do número secreto!");
+    palpite=parseInt(window.prompt("Digite um número entre 1 e "+limiteNumeroSecreto));
+    if(palpite>numeroSecreto){
+        window.alert("Quase lá! Tente um número menor");
+    }else if(palpite<numeroSecreto){
+        window.alert("Poxa, não foi desta vez... tente um número maior");
+    }
+}while(palpite!=numeroSecreto);
